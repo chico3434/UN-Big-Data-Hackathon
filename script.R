1e385b132206646b29d19a6e791caed9f21cf99e
andre <andrelessa90@gmail.com>
2022-11-09 14:30:19 -0300

    reading data base

--- script.R
+++ script.R
@@ -1,6 +1,7 @@
+data <- read.csv('base.csv')
+
 data.with.deaths <- data[!is.na(data$Total.Deaths),]
 
 data.with.deaths.affected <- data.with.deaths[!is.na(data.with.deaths$Total.Affected),]
 
 brazil.with.deaths.affected <- data.with.deaths.affected[which(data.with.deaths.affected$Country == 'Brazil'),]
-  
